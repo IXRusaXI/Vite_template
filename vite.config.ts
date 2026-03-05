dacc7d94273faee124470900e56b5b9196c261bd
Руслан <moi-denga@yandex.ru>
2026-03-05 21:02:03 +0300

    Add Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
   plugins: [
     react({
       babel: {
